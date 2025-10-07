a0be05f35ebfdbe5edc962d20b17691e0b3636a5
Robin_Bng <robin.bengtsson@hotmail.se>
2025-10-07 10:37:02 +0200

    gjort uppgiften

--- genrep.py
+++ genrep.py
@@ -1,1 +1,18 @@
+#Author: Robin Bengtsson
+# Date: 2025-10-07
+
+serie = 0
+parallell = 0
+
 print("Ei24 - genrep praktiskt prov ht25")
+resistors = input(f"Ange resistorer: ")
+
+resistor_list = resistors.split()
+
+for i in resistor_list:
+    serie += int(i)
+    parallell += (1 / int(i))
+parallell = 1 / parallell
+
+print(f"Serieresistans: {serie}")
+print(f"Parallellresistans: {parallell}")
